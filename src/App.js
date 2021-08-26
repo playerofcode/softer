952deae7a9a6c71f1bd03a52aa6e7de7365e0b3b
Player Of Code <playerofcode@gmail.com>
2021-08-26 17:59:10 +0530

    260820211758

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import Navbar from './components/Navbar'
+import {Switch,Route } from "react-router-dom";
+import About from './components/About'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Error from './components/Error'  
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Switch>
+    <Route exact path='/' component={Home} />
+    <Route exact path='/contact' component={Contact} />
+    <Route exact path='/about' component={About} />
+    <Route  component={Error} />
+    </Switch>
     </>
   );
 }
