85edcd77f06c84011ff8643b2cfb210bdfd02e33
Player Of Code <playerofcode@gmail.com>
2021-08-27 11:43:22 +0530

    270820211143

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,39 @@
 import React, { useState } from 'react';
-
+import {Link } from "react-router-dom";
 function Navbar() {
     const [show, setShow] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
     return (
         <>
       
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Player Of Code</a>
+          <Link className="navbar-brand" to="/">Player Of Code</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={()=>setShow(!show)}>
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className={`collapse navbar-collapse ${show ? "show" : ""}`}>
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
+                <Link className="nav-link" to="about">About</Link>  
               </li>
+              <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={()=>setDropdown(!dropdown)}>
+                Links
+              </a>
+              <ul className={`dropdown-menu ${dropdown ? "show" : ""}`} aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" to="#">Hostinger</a></li>
+                <li><a className="dropdown-item" to="#">Text Local</a></li>
+                <li><a className="dropdown-item" to="#">Razorpay</a></li>
+              </ul>
+            </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="contact">Contact</Link>  
+              </li>
+             
             </ul>
 
           </div>
